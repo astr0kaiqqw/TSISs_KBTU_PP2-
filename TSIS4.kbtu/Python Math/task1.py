f76e075e45d23c69718fd8d753ab3be4e57a994b
Ruslan Taikpanov <rtaikpanov@mail.ru>
2025-02-16 17:59:15 +0500

    tsis 4

---
+++ TSIS4.kbtu/Python Math/task1.py
@@ -0,0 +1,9 @@
+import math
+
+def degree_to_radian(degree):
+    return degree * (math.pi / 180)
+
+if __name__ == "__main__":
+    degree = float(input("Input degree: "))
+    radian = degree_to_radian(degree)
+    print("Output radian:", round(radian, 6))
