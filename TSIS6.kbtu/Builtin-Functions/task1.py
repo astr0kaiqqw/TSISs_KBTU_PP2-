5a9af837f46e09d533044d247910765755e75d3b
Ruslan Taikpanov <rtaikpanov@mail.ru>
2025-02-25 12:04:34 +0500

    tsis 6

---
+++ TSIS6.kbtu/Builtin-Functions/task1.py
@@ -0,0 +1,9 @@
+import math
+
+def multiply_list(numbers):
+    return math.prod(numbers)
+
+
+nums = [2, 3, 4, 5]
+result = multiply_list(nums)
+print("Произведение всех чисел в списке:", result)
---
+++ TSIS6.kbtu/Builtin-Functions/task5.py
@@ -0,0 +1,9 @@
+def all_true(t):
+    return all(t)
+
+
+t1 = (True, True, True)
+t2 = (True, False, True)
+
+print("Все элементы в t1 True?", all_true(t1))  
+print("Все элементы в t2 True?", all_true(t2))  
